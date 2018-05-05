9b43cab3aee895518accb73c5398a08757f6f036
Dominik Picheta <dominikpicheta@googlemail.com>
2018-05-05 21:14:48 +0100

    Workaround VM bug in strutils

--- lib/pure/strutils.nim
+++ lib/pure/strutils.nim
@@ -17,8 +17,9 @@ import parseutils
 from math import pow, round, floor, log10
 from algorithm import reverse
 
-from unicode import toLower, toUpper
-export toLower, toUpper
+when not defined(nimvm):
+  from unicode import toLower, toUpper
+  export toLower, toUpper
 
 {.deadCodeElim: on.}  # dce option deprecated
 
